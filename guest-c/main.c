2d32eda0b71a0153036eb8450fa038c3b33353d8
Christof Petig <christof.petig@arcor.de>
2023-09-08 23:15:52 +0200

    correct the C example - it freed the object twice

--- guest-c/main.c
+++ guest-c/main.c
@@ -7,7 +7,7 @@ int main() {
     test_example_my_interface_method_my_object_set(o2, 17);
     test_example_my_interface_method_my_object_set(o2, test_example_my_interface_method_my_object_get(o2)*2);
     printf("Object is %d\n", test_example_my_interface_method_my_object_get(o2));
-    my_world_my_object_drop_borrow(o2);
+    // my_world_my_object_drop_borrow(o2); - this would drop it twice
     test_example_my_interface_my_object_drop_own(o);
     return 0;
 }
